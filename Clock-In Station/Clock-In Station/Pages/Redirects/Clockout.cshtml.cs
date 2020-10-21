03b20673c54b0b98e7d41e566f51e99f603ae75c
Orlando <lopezorlando23@gmail.com>
2020-10-21 10:56:42 -0600

    Added pages
    Added the clockin and clock out pages so that the user can be redirected on a post action.
    also updated the index page with an additional picture for log in.

---
+++ Clock-In Station/Clock-In Station/Pages/Redirects/Clockout.cshtml.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Clock_In_Station.Pages.Redirects
+{
+    public class ClockoutModel : PageModel
+    {
+
+        public string Message { get; set; }
+        public void OnGet()
+        {
+            Message = "You are clocked out!";
+        }
+    }
+}
