3aa0e88cd38bdae906177632f0b8acf988f3c8a5
Orlando <lopezorlando23@gmail.com>
2020-10-23 11:41:27 -0600

    Added Employee.Data class library
    employee.data class library was created to facilitate the data writing in this application

---
+++ Clock-In Station/Employee.Data/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee.Data
+{
+    public class Employee
+    {
+        public int employeeID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
