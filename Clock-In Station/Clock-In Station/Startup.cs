967d218afcdcf1ac11d29b8cce4cbd73c21e8b2f
Orlando <lopezorlando23@gmail.com>
2020-10-26 10:59:32 -0600

    Slight Updates

--- Clock-In Station/Clock-In Station/Startup.cs
+++ Clock-In Station/Clock-In Station/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Employee.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +24,7 @@ namespace Clock_In_Station
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IEmployeeData, InMemoryEmployeeData>();
             services.AddRazorPages();
         }
 
